3b91b4f97b47ab9db2fe06e5f34cf3dc9eb36e32
gliberov <124058342+gliberov@users.noreply.github.com>
2023-11-14 13:13:25 -0500

    Update characters.js

---
+++ vite-project/characters.js
@@ -0,0 +1,58 @@
+const characters = [
+    {
+        name: "Yoda",
+        img: "placeholder",
+        lightsaberColor: "Green",
+        jedi: true,
+        sith: false
+    },
+    {
+        name: "Obi-Wan Kenobi",
+        img: "placeholder",
+        lightsaberColor: "Blue",
+        jedi: true,
+        sith: false
+    },
+    {
+        name: "Mace Windu",
+        img: "placeholder",
+        lightsaberColor:"Purple",
+        jedi: true,
+        sith: false
+    },
+    {
+        name: "Luke Skywalker",
+        img: "placeholder",
+        lightsaberColor:"Blue",
+        jedi: true,
+        sith: false
+    },
+    {
+        name: "Darth Maul",
+        img: "placeholder",
+        lightsaberColor:"Red",
+        jedi: false,
+        sith: true
+    },
+    {
+        name: "Count Dookie",
+        img: "placeholder",
+        lightsaberColor:"Red",
+        jedi: false,
+        sith: true
+    },
+    {
+        name: "Anakin Skywalker",
+        img: "placeholder",
+        lightsaberColor: ["blue", "red"],
+        jedi: true,
+        sith: true
+    },
+    {
+        name: "Yoda",
+        img: "placeholder",
+        lightsaberColor:"green",
+        jedi: true,
+        sith: false
+    }
+]
